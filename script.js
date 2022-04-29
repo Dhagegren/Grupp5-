83c2fc1acda0c6878ffc15c07e40141c96b7a872
Labantv <linuslabanhd@gmail.com>
2022-04-29 11:47:02 +0200

    Update script.js
    Städa upp kod, la till kommentarer

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 
-var modal = document.getElementsByClassName("modal");
+var modal = document.getElementsByClassName("modal");//Reference to modal
 
 function init() {
-	let btn = document.getElementsByClassName("myBtn");
-	let span = document.getElementsByClassName("close")[0];
-
-	console.log(modal[0]);
-	
+	let btn = document.getElementsByClassName("myBtn");//Reference to button
+	let span = document.getElementsByClassName("close")[0];//Reference to span element
+	//Make all buttons clickable
 	for (let i = 0; i < btn.length; i++) {
 		btn[i].addEventListener("click", showModal);
 	}
@@ -14,17 +12,17 @@ function init() {
 }
 window.onload = init;
 
-// When the user clicks on the button, open the modal
+//Open up modal
 function showModal() {
 	modal[0].style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+//Close modal
 function hideModal() {
 	modal[0].style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+//Click outside modal to close
 window.onclick = function(event) {
 	if (event.target == modal[0]) {
 		modal[0].style.display = "none";
